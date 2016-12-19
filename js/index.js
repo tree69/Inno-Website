318e5d8de249d932340b86e4bc3f56a1b7b91485
Anh Bui <mr.anhbui69@gmail.com>
2016-12-19 16:53:54 +0700

    19_12_2016

---
+++ js/index.js
@@ -0,0 +1,220 @@
+
+
+
+/* --------------------------------------- */
+
+
+
+function create_custom_dropdowns() {
+  $('select').each(function(i, select) {
+    if (!$(this).next().hasClass('dropdown')) {
+      $(this).after('<div class="dropdown ' + ($(this).attr('class') || '') + '" tabindex="0"><span class="current"></span><div class="list"><ul></ul></div></div>');
+      var dropdown = $(this).next();
+      var options = $(select).find('option');
+      var selected = $(this).find('option:selected');
+      dropdown.find('.current').html(selected.data('display-text') || selected.text());
+      options.each(function(j, o) {
+        var display = $(o).data('display-text') || '';
+        dropdown.find('ul').append('<li class="option ' + ($(o).is(':selected') ? 'selected' : '') + '" data-value="' + $(o).val() + '" data-display-text="' + display + '">' + $(o).text() + '</li>');
+      });
+    }
+  });
+}
+
+// Event listeners
+
+// Open/close
+$(document).on('click', '.dropdown', function(event) {
+  $('.dropdown').not($(this)).removeClass('open');
+  $(this).toggleClass('open');
+  if ($(this).hasClass('open')) {
+    $(this).find('.option').attr('tabindex', 0);
+    $(this).find('.selected').focus();
+  } else {
+    $(this).find('.option').removeAttr('tabindex');
+    $(this).focus();
+  }
+});
+// Close when clicking outside
+$(document).on('click', function(event) {
+  if ($(event.target).closest('.dropdown').length === 0) {
+    $('.dropdown').removeClass('open');
+    $('.dropdown .option').removeAttr('tabindex');
+  }
+  event.stopPropagation();
+});
+// Option click
+$(document).on('click', '.dropdown .option', function(event) {
+  $(this).closest('.list').find('.selected').removeClass('selected');
+  $(this).addClass('selected');
+  var text = $(this).data('display-text') || $(this).text();
+  $(this).closest('.dropdown').find('.current').text(text);
+  $(this).closest('.dropdown').prev('select').val($(this).data('value')).trigger('change');
+});
+
+// Keyboard events
+$(document).on('keydown', '.dropdown', function(event) {
+  var focused_option = $($(this).find('.list .option:focus')[0] || $(this).find('.list .option.selected')[0]);
+  // Space or Enter
+  if (event.keyCode == 32 || event.keyCode == 13) {
+    if ($(this).hasClass('open')) {
+      focused_option.trigger('click');
+    } else {
+      $(this).trigger('click');
+    }
+    return false;
+    // Down
+  } else if (event.keyCode == 40) {
+    if (!$(this).hasClass('open')) {
+      $(this).trigger('click');
+    } else {
+      focused_option.next().focus();
+    }
+    return false;
+    // Up
+  } else if (event.keyCode == 38) {
+    if (!$(this).hasClass('open')) {
+      $(this).trigger('click');
+    } else {
+      var focused_option = $($(this).find('.list .option:focus')[0] || $(this).find('.list .option.selected')[0]);
+      focused_option.prev().focus();
+    }
+    return false;
+  // Esc
+  } else if (event.keyCode == 27) {
+    if ($(this).hasClass('open')) {
+      $(this).trigger('click');
+    }
+    return false;
+  }
+});
+
+$(document).ready(function() {
+  create_custom_dropdowns();
+});
+
+
+/* ------------------------------------ */
+$(window).load(function(){
+  bannerPos();
+});
+
+$(window).resize(function(){
+  bannerPos();
+});
+
+function bannerPos() {
+  var windowWidth = $(window).outerWidth();
+  var contentWidth = $('.content').outerWidth();
+  var bannerWidth = 170;
+  //var sideSpace = (windowWidth - contentWidth) / 2;
+  //var pos = sideSpace - bannerWidth;
+  //$('.banner.left').css('left', pos + 'px');
+  //$('.banner.right').css('right', pos + 'px');
+}
+
+/* ------------------------------------ */
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+
+/* ------------------------------------ */
+jQuery(document).ready(function($){
+	//open popup
+	$('.cd-popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
+
+
+jQuery(document).ready(function($){
+	//open popup
+	$('.cd-popup-trigger1').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
+
+
+/* ------------------------------------ */
+$(document).ready(function() {
+  $('.video-gallery').magnificPopup({
+  delegate: 'a', 
+  type: 'iframe',
+  gallery:{
+    enabled:true
+  }
+});
+});
+
+
+/* ------------------------------------ */
+$('#team-slider').flickity({
+  // options
+  cellAlign: 'center',
+  contain: true,
+  wrapAround: true,
+  //imagesLoaded: true,
+  percentPosition: true,
+  pageDots: false,
+  watchCSS: true
+});
+
+$('#team-slider1').flickity({
+  // options
+  cellAlign: 'center',
+  contain: true,
+  wrapAround: true,
+  //imagesLoaded: true,
+  percentPosition: true,
+  pageDots: false,
+  watchCSS: true
+});
+
